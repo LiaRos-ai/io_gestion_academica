2667212c2b58113ef4b11614be5bfadb26a14dbf
WebSluisj <lusij.alvaradoq@gmail.com>
2025-09-11 10:19:38 -0400

    Agrega archivos pendientes antes de la fusión

---
+++ .angular/cache/20.3.0/app/vite/deps/md.transition-EWYZEDZD.js
@@ -0,0 +1,12 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-UPSTTTRG.js";
+import "./chunk-YZWDY2VY.js";
+import "./chunk-PHINHIIC.js";
+import "./chunk-QEE7QVES.js";
+import "./chunk-KU2B7TFN.js";
+import "./chunk-DVQC2WOX.js";
+import "./chunk-EXLSCMNP.js";
+export {
+  mdTransitionAnimation
+};
